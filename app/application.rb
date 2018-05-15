656ef900bad9469b33116af801bc35809100e7e3
Annie Bourke <aabourke@gmail.com>
2018-05-15 00:12:50 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+    #if req.path=="/items"
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+
+      resp.write song.artist
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
