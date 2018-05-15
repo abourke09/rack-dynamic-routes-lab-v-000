c3391501e68335c7419258afefc9ac2cf0d1e525
Annie Bourke <aabourke@gmail.com>
2018-05-15 00:38:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,6 @@ class Application
         resp.write "Item not found"
         resp.status = 400
       end
-      resp.write song.artist
     else
       resp.write "Route not found"
       resp.status = 404
