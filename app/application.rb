4936aff7f7951c978021fb550bc0680190ef9fa8
Annie Bourke <aabourke@gmail.com>
2018-05-15 00:49:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,9 @@ class Application
     if req.path.match(/items/)
     #if req.path=="/items"
       item_name = req.path.split("/items/").last
-      price = @@items.find {|x| x[:name] == item_name}
-
+      object = @@items.find {|x| x[:name] == item_name}
+      price = object[:price]
+      
       if @@items.include?(item_name)
         resp.write "#{price}"
       else
